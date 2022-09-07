01da791946c91238d7527858026080adc23fea56
CarlosMantovani <111471011+CarlosMantovani@users.noreply.github.com>
2022-09-07 12:20:43 -0300

    atualizado

--- Cadastro/main.js
+++ Cadastro/main.js
@@ -30,27 +30,6 @@ inputs.forEach(input => {
 	input.addEventListener("blur", remcl);
 });
 
-function Formatacpf(Campo, teclapres) {
-	
-	var tecla = teclapres.keyCode;
-	var vr = new String(Campo.value);
-	vr = vr.replace(".", "");
-	vr = vr.replace(".", "");
-	vr = vr.replace(".", "");
-	vr = vr.replace(".", "");
-	tam = vr.length + 1;
-		 
-	if (tecla != 8 && tecla != 8) {
-		 
-	   if (tam > 0 && tam < 2)
-		   Campo.value = vr.substr(0, 2) ;
-	   if (tam > 2 && tam < 4)
-	   Campo.value = vr.substr(0, 2) + '/' + vr.substr(2, 2);
-	   if (tam > 4 && tam < 7)
-	   Campo.value = vr.substr(0, 2) + '/' + vr.substr(2, 2) + '/' + vr.substr(4, 7);
-	}
- 
- }
 
 
 function Formatadata(Campo, teclapres) {
@@ -63,9 +42,8 @@ function Formatadata(Campo, teclapres) {
 	tam = vr.length + 1;
 		 
 	if (tecla != 8 && tecla != 8) {
-		 
 	   if (tam > 0 && tam < 2)
-		   Campo.value = vr.substr(0, 2) ;
+		   Campo.value = vr.substr(0, 2) 
 	   if (tam > 2 && tam < 4)
 	   Campo.value = vr.substr(0, 2) + '/' + vr.substr(2, 2);
 	   if (tam > 4 && tam < 7)
@@ -73,17 +51,27 @@ function Formatadata(Campo, teclapres) {
 	}
  
  }
- 
-function FormataCPF(){   
 
-    var telefone = document.getElementById('telefone')
-	if(telefone.value.length ==0){
-		cpf.value += "("
-}else if(cpf.value.length == 3){
-	cpf.value += ")"
-}else if(cpf.value.length == 11){
+function mask(o, f) {
+  setTimeout(function() {
+    var v = mphone(o.value);
+    if (v != o.value) {
+      o.value = v;
+    }
+  }, 1);
+}
 
-		cpf.value += "-"
-	}
-    
+function mphone(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+    r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+    r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+    r = r.replace(/^(\d*)/, "($1");
+  }
+  return r;
 }
